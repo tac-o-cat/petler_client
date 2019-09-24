d11c66a20c378bf0a309a4ff38bda2b86ee64fe6
ahndohun <ahndohun1@gmail.com>
2019-09-24 13:40:35 +0900

    :heavy_plus_sign: Add storybook and btn compoenent

---
+++ src/components/button/button.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "./Button";
+
+export default { title: "Button" };
+
+export const defaultButton = () => <Button />;
+
+// components/button
+// ㄴ button.js
+// ㄴ button.js
+// ㄴ button.stories.js
