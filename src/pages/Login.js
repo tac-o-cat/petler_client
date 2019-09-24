5f30298b21e39c2d48a977f828ab98891f7fa261
Mini Kim <mini150146@gmail.com>
2019-09-24 15:34:34 +0900

    :sparkles: Add basic components for Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Input from "../components/input/Input";
+import Button from "../components/button/Button";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h3>나는 집사다</h3>
+      <form>
+        <Input placeholder="E-mail" />
+        <Input placeholder="Password" type="password" />
+        <Button text="Login" />
+      </form>
+      <span>회원가입/비밀번호 찾기</span>
+      <Button text="Login with Google" />
+      <Button text="Login with Kakao" />
+    </div>
+  );
 };
 
 export default Login;
