651932b2c6c036deb3bddd42f2a507303a32eb5d
Mini Kim <mini150146@gmail.com>
2019-09-24 19:39:07 +0900

    :sparkles: Add route to SignUp and FindPassword

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,4 @@
 export { default as Login } from "./Login";
 export { default as NotFound } from "./NotFound";
+export { default as SignUp } from "./SignUp";
+export { default as FindPassword } from "./FindPassword";
