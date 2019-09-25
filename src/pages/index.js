bf04d20227859c326b9348d48e23a7e68a53122e
Mini Kim <mini150146@gmail.com>
2019-09-25 10:32:04 +0900

    :sparkles: Add routes for login, signup, findpw pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,4 @@
 export { default as Login } from "pages/Login";
 export { default as NotFound } from "pages/NotFound";
+export { default as SignUp } from "pages/SignUp";
+export { default as FindPassword } from "pages/FindPassword";
