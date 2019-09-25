05a5147c05bc9468279b93ad9b76280df8328509
Mini Kim <mini150146@gmail.com>
2019-09-25 10:33:26 +0900

    :sparkles: Add components for SignUp page

---
+++ src/pages/SignUp.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Input from "../components/input/Input";
+import Button from "../components/button/Button";
+
+const SignUp = ({ history }) => {
+  return (
+    <div>
+      <h3>회원가입</h3>
+      <form>
+        <Input placeholder="E-mail" />
+        <Input placeholder="Username" />
+        <Input placeholder="Password" type="password" />
+        <Input placeholder="Confirm Password" type="password" />
+        <Button text="회원 가입" />
+      </form>
+      <Button text="뒤로 가기" onClick={history.goBack} />
+    </div>
+  );
+};
+
+SignUp.propTypes = {
+  history: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default SignUp;
