94f0a1dfe40b9c4c8d1946c876238648e2b46c35
Mini Kim <mini150146@gmail.com>
2019-09-25 10:34:10 +0900

    :sparkles: Add Link to signup and findpassword routes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Input from "../components/input/Input";
 import Button from "../components/button/Button";
 
@@ -11,7 +12,12 @@ const Login = () => {
         <Input placeholder="Password" type="password" />
         <Button text="Login" />
       </form>
-      <span>회원가입/비밀번호 찾기</span>
+      <Link to="/signup">
+        <span>회원가입</span>
+      </Link>
+      <Link to="/findpassword">
+        <span>비밀번호 찾기</span>
+      </Link>
       <Button text="Login with Google" />
       <Button text="Login with Kakao" />
     </div>
