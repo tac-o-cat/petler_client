ac43f668751291d706a72d39ed98f86cd7887252
ahndohun <ahndohun1@gmail.com>
2019-09-26 21:08:04 +0900

    :art: Modify isLogin state for conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
-import { Login, NotFound, SignUp, FindPassword } from "pages";
+import { Login, NotFound, SignUp, FindPassword, Main } from "pages";
+import LayoutWrapper from "layout/LayoutWrapper";
 
 function App() {
+  const [isLogin] = useState(false);
+
   return (
     <Switch>
-      <Route path="/" exact component={Login} />
-      <Route path="/signup" component={SignUp} />
-      <Route path="/findpassword" component={FindPassword} />
+      {isLogin ? (
+        <LayoutWrapper>
+          <Route path="/main" component={Main} />
+        </LayoutWrapper>
+      ) : (
+        <>
+          <Route path="/" exact component={Login} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/findpassword" component={FindPassword} />
+        </>
+      )}
       <Route component={NotFound} />
     </Switch>
   );
