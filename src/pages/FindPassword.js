4811316c76429f3156f8aa4c582496571b0f8f00
Mini Kim <mini150146@gmail.com>
2019-09-27 10:34:46 +0900

    :lipstick: Update UI for Login, SignUp, FIndPassword
    mui 컴포넌트 적용

--- src/pages/FindPassword.js
+++ src/pages/FindPassword.js
@@ -1,24 +1,79 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 
 const FindPassword = ({ history }) => {
+  const useStyles = makeStyles(theme => ({
+    "@global": {
+      body: {
+        backgroundColor: theme.palette.common.white,
+      },
+    },
+    paper: {
+      marginTop: theme.spacing(20),
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    form: {
+      width: "100%",
+      marginTop: theme.spacing(10),
+    },
+    submit: {
+      margin: theme.spacing(1, 0, 0),
+    },
+  }));
+  const classes = useStyles();
   return (
-    <div>
-      <h3>비밀번호 찾기</h3>
-      <form>
-        <input placeholder="E-mail" />
-        <button type="button">확인</button>
-      </form>
-      <button type="button" onClick={history.goBack}>
-        뒤로 가기
-      </button>
-    </div>
+    <Container maxWidth="xs">
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          비밀번호 찾기
+        </Typography>
+        <ValidatorForm className={classes.form} ref={() => "form"}>
+          <TextValidator
+            label="이메일"
+            name="email"
+            validators={["required", "isEmail"]}
+            errorMessages={["이메일을 입력해 주세요", "올바른 이메일 주소를 입력해 주세요"]}
+            autoComplete="email"
+            autoFocus
+            fullWidth
+            type="email"
+            variant="outlined"
+            margin="dense"
+          />
+          <Button
+            fullWidth
+            className={classes.submit}
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+            확인
+          </Button>
+        </ValidatorForm>
+        <Button
+          fullWidth
+          className={classes.submit}
+          variant="contained"
+          color="primary"
+          onClick={() => history.push("/")}
+        >
+          뒤로 가기
+        </Button>
+      </div>
+    </Container>
   );
 };
 
 FindPassword.propTypes = {
   history: PropTypes.shape({
-    goBack: PropTypes.func.isRequired,
+    push: PropTypes.func.isRequired,
   }).isRequired,
 };
 
