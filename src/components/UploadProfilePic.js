ed79b8c09c7895161fa73f0f318c22557a177fd2
Mini Kim <mini150146@gmail.com>
2019-09-27 10:35:58 +0900

    :sparkles: Create UploadProfilePic component

---
+++ src/components/UploadProfilePic.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ImagesUploader from "react-images-uploader";
+import "react-images-uploader/styles.css";
+
+const UploadProfilePic = () => {
+  // url에 POST request 보낼 주소 넣기.
+  // https://github.com/aleksei0807/react-images-uploader
+  return (
+    <ImagesUploader
+      url="localhost:3000"
+      optimisticPreviews
+      multiple={false}
+      onLoadEnd={err => {
+        if (err) {
+          /* eslint-disable no-console */
+          console.error(err);
+        }
+      }}
+    />
+  );
+};
+
+export default UploadProfilePic;
