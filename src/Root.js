346c80705e60597eee36573e56abec68bae4cee0
Mini Kim <mini150146@gmail.com>
2019-09-28 20:30:42 +0900

    :sparkles: Add apolloProvider
    apolloclient를 클라이언트에 붙여(?)주었습니다

--- src/Root.js
+++ src/Root.js
@@ -3,6 +3,12 @@ import { BrowserRouter } from "react-router-dom";
 import App from "App";
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
 
 const theme = createMuiTheme({
   typography: {
@@ -12,11 +18,13 @@ const theme = createMuiTheme({
 
 function Root() {
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
