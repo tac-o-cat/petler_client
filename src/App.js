326c3cd1255277a3f88b28c67ca698df22718a7a
Mini Kim <mini150146@gmail.com>
2019-10-04 17:55:32 +0900

    :sparkles: Add global store by using context API
    authentication에 context api를 적용하여, 로그인 후에 접근할 수 있는 global store를 만들었습니다.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { Login, NotFound, SignUp, FindPassword, CreateChannel, Main, MyPage } from "pages";
 import LayoutWrapper from "layout/LayoutWrapper";
-import AuthenticatedComponent from "components/AuthenticatedComponent";
+import { Authentication } from "components/Authentication";
 
 function App() {
   // 문제: notFound로 접근이 안 되고 있음.
@@ -11,13 +11,13 @@ function App() {
       <Route path="/" exact component={Login} />
       <Route path="/signup" component={SignUp} />
       <Route path="/findpassword" component={FindPassword} />
-      <AuthenticatedComponent>
+      <Authentication>
         <Route path="/createChannel" component={CreateChannel} />
         <LayoutWrapper>
-          <Route path="/mypage" component={MyPage} />
           <Route path="/main" component={Main} />
+          <Route path="/mypage" component={MyPage} />
         </LayoutWrapper>
-      </AuthenticatedComponent>
+      </Authentication>
       <Route component={NotFound} />
     </Switch>
   );
